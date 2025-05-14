f5655aef4383527639caaf1a428ad41f439c7017
uchida189 <perukipoo@icloud.com>
2025-05-14 12:05:06 +0900

    [feat]コミット

---
+++ app/(pages)/hello/page.tsx
@@ -0,0 +1,12 @@
+export default function Hello() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div className="flex rounded-[20] h-100 w-100 items-center justify-center bg-black/30 shadow-[0px_0px_100px_5px] shadow-white/10">
+          aaa
+        </div>
+        <div className="bg-blue-500">aaaa</div>
+      </main>
+    </div>
+  );
+}
