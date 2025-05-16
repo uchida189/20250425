f17dd827e26abc15d76f92b40ce9a73c5639cb2b
taminororo <169162271+taminororo@users.noreply.github.com>
2025-05-16 11:23:50 +0900

    [feat]<kambayashi>文字を上下左右の中央に配置しました #1

--- app/page.tsx
+++ app/page.tsx
@@ -1,166 +1,94 @@
-import Image from "next/image";
+// app/page.tsx
+
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        {/* <div className="flex rounded-[20] h-100 w-100 items-center justify-center bg-black/50 border-3 border-white/20 p-10 outline-offset-0 outline-black/50 outline-4 backdrop-blur-lg shadow-lg shadow-white/50"> */}
-        <div className="flex rounded-[20] h-100 w-100 items-center justify-center bg-black/30 shadow-[0px_0px_100px_5px] shadow-white/10">
-          aaa
-        </div>
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        
-        
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  
+
+
+  return (
+
+      <div>
+        <div className="w-full h-24 mt-9 bg-white text-indigo-500">
+          <div className="bg-gradient-to-r from-orange-500 via-yellow-500 to-fuchsia-500 bg-clip-text text-2xl font-bold text-transparent">
+              まず、寄せを使わないとこんな感じ
+          </div>
+          <div className="bg-gradient-to-r from-orange-500 via-yellow-500 to-fuchsia-500 bg-clip-text text-2xl font-bold text-transparent">
+              The quick brown fox jumps over the lazy dog
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="w-full h-20 bg-black flex items-center border-3 border-red-400">
+            <p className="bg-gradient-to-r from-green-500 via-emerald-500 to-cyan-500 bg-clip-text text-2xl font-bold text-transparent">
+              これが上下中央揃え
+            </p>         
+        </div>
+        <div>
+          <div className="w-full h-20 bg-black flex items-center border-3 border-lime-400">
+            <p className="bg-gradient-to-r from-green-500 via-emerald-500 to-cyan-500 bg-clip-text text-2xl font-bold text-transparent">
+              The quick brown fox jumps over the lazy dog
+            </p>  
+          </div>
+        </div>
+        
+        <div className="w-full h-20 bg-black text-center">
+          <p className="bg-gradient-to-r from-cyan-500 via-indigo-500 to-purple-500 bg-clip-text text-2xl font-bold text-transparent">
+            これが左右中央揃え
+          </p>
+          <p className="bg-gradient-to-r from-cyan-500 via-indigo-500 to-purple-500 bg-clip-text text-2xl font-bold text-transparent">
+            The quick brown fox jumps over the lazy dog
+          </p>
+        </div>
+        <div className="w-full h-48  bg-white text-indigo-500 flex items-center flex-col justify-center">
+          <div className="bg-gradient-to-r from-orange-500 via-yellow-500 to-fuchsia-500 bg-clip-text text-2xl font-bold text-transparent">
+            最後に、上下左右中央揃え！
+          </div>
+          <div className="bg-gradient-to-r from-orange-500 via-yellow-500 to-fuchsia-500 bg-clip-text text-2xl font-bold text-transparent">
+            The quick brown fox jumps over the lazy dog
+          </div>
+          <div className="bg-gradient-to-r from-orange-500 via-yellow-500 to-fuchsia-500 bg-clip-text text-2xl font-bold text-transparent">
+            The quick brown fox jumps over the lazy dog
+          </div>
+          <div className="bg-gradient-to-r from-orange-500 via-yellow-500 to-fuchsia-500 bg-clip-text text-2xl font-bold text-transparent">
+            The quick brown fox jumps over the lazy dog
+          </div>
+        </div>
+        <div className="w-full h-48 bg-black flex items-center flex-wrap justify-center">
+          <p className="font-pacifico text-2xl">Pacifico!</p>
+
+          <p className="font-ubuntu font-bold">Ubuntu Bold</p>
+
+          <p className="font-zenmaru italic">Zen Maru Gothic Italic</p>
+
+          <p className="font-sharetech text-lg">Share Tech Regular</p>
+
+          <p className="font-mono">
+            メロスは激怒した。必ず、かの邪智暴虐の王を除かなければならぬと決意した。
+          </p>
+          <p className="font-serif">
+            ある日の暮方の事である。一人の下人が、羅生門の下で雨やみを待っていた。　広い門の下には、この男のほかに誰もいない。
+          </p>
+        </div>
+        <div className="bg-yellow-500 p-5 mt-3 border-8 border-white/50 outline-4 outline-blue-100 text-center">
+          aaaa
+        </div>
+        <div className="w-96 h-96 bg-white">
+          <p className="text-blue-600 dark:text-sky-400/100">The quick brown fox jumps over the lazy dog</p>
+          <p className="text-blue-500 dark:text-sky-400/100">The quick brown fox jumps over the lazy dog</p>
+          <p className="text-blue-400 dark:text-sky-400/100">The quick brown fox jumps over the lazy dog</p>
+          <p className="text-blue-100 dark:text-sky-400/100">The quick brown fox jumps over the lazy dog</p>
+        </div>
+        <div>
+          <div className="border-8 border-sky-400 bg-indigo-500 bg-clip-border p-3">xxx</div>
+          
+          <div className="border-8 border-black bg-indigo-500 bg-clip-padding p-3">xxx</div>
+          <div className="border-4 bg-indigo-500 bg-clip-content p-3">xxx</div>
+          <p className="bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-5xl font-extrabold text-transparent ...">
+            Hello world
+          </p>
+        </div>
+      </div>
+      
+  
   );
 }
